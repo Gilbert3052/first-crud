d0e6ea667637616d90416ee33ea38a601028180f
Gilbert3052 <gilbertfernandez3052@gmail.com>
2022-10-09 18:55:10 -0400

    add link github

--- src/assets/Title/Title.jsx
+++ src/assets/Title/Title.jsx
@@ -5,7 +5,7 @@ const Title = () => {
   return (
     <div className="Title">
         <h1>User CRUD - REACT</h1>
-        <a href="https://github.com/" target={"_blank"}><box-icon type='logo' name='github' color="#8FE3CF"></box-icon></a>
+        <a href="https://github.com/Gilbert3052/first-crud" target={"_blank"}><box-icon type='logo' name='github' color="#8FE3CF"></box-icon></a>
     </div>
   )
 }
